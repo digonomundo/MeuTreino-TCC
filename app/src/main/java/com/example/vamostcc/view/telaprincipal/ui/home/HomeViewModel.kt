c55fb68aeb9f7ecf51e56c35babcbc6f18f42565
Rodrigo Marques Dias <rodrigo.dias60@etec.sp.gov.br>
2023-09-10 21:57:59 -0300

    tela principal adicionada

---
+++ app/src/main/java/com/example/vamostcc/view/telaprincipal/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.vamostcc.view.telaprincipal.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/example/vamostcc/view/telaprincipal/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.vamostcc.view.telaprincipal.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
