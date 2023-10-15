2bc05902c399cd283a1ddf1b7949f1dd94915ddc
Rodrigo Marques Dias <rodrigo.dias60@etec.sp.gov.br>
2023-10-15 00:17:53 -0300

    Correções de erros e transferências de Layout para fragments

--- app/src/main/java/com/example/vamostcc/view/telaprincipal/telaPrincipal.kt
+++ app/src/main/java/com/example/vamostcc/view/telaprincipal/telaPrincipal.kt
@@ -28,10 +28,7 @@ class telaPrincipal : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarTelaPrincipal.toolbar)
 
-        binding.appBarTelaPrincipal.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_tela_principal)
